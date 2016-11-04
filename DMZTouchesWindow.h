627dbd90cc861a5cfb3cc15fc5d584dfc19b2ed4
dmoroz0v <denis.moroz.morozov@gmail.com>
2016-11-04 16:24:01 +0700

    Support force touch

--- DMZTouchesWindow.h
+++ DMZTouchesWindow.h
@@ -7,6 +7,8 @@
 
 #import <UIKit/UIWindow.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface DMZTouchesWindow : UIWindow
 
 // Color of touch circles. Default is black with 50% opacity
@@ -19,4 +21,14 @@
 // NO - touch circles are not visible. Defailt is NO
 @property (nonatomic, assign) CGFloat dmz_touchesEnabled;
 
+// Color of touch force circles. Default is nil.
+// If is nil, then will light color of dmz_touchesColor
+@property (nonatomic, strong, nullable) UIColor *dmz_touchesForceColor;
+
+// Determines visible touch force circles. YES - touch force circles are no visible.
+// NO - touch force circles are visible. Defailt is NO
+@property (nonatomic, assign) CGFloat dmz_touchesForceDisabled;
+
 @end
+
+NS_ASSUME_NONNULL_END
--- DMZTouchesWindow.m
+++ DMZTouchesWindow.m
@@ -7,10 +7,69 @@
 
 #import "DMZTouchesWindow.h"
 
+// MARK: DMZTouchView
+
+@interface DMZTouchView : UIView
+
+- (void)setCoreCollor:(UIColor *)coreColor;
+- (void)setForceCollor:(UIColor *)forceColor;
+- (void)setForceRadius:(CGFloat)forceRadius;
+
+@end
+
+@implementation DMZTouchView
+{
+	UIView *_core;
+	UIView *_force;
+}
+
+- (instancetype)initWithRadius:(CGFloat)radius
+{
+	CGRect frame = CGRectMake(0, 0, radius * 2, radius * 2);
+
+	self = [super initWithFrame:frame];
+
+	_force = [[UIView alloc] initWithFrame:(CGRect){CGPointZero, frame.size}];
+	_force.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
+	_force.layer.masksToBounds = YES;
+	_force.layer.cornerRadius = radius;
+	[self addSubview:_force];
+
+	_core = [[UIView alloc] initWithFrame:(CGRect){CGPointZero, frame.size}];
+	_core.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
+	_core.layer.masksToBounds = YES;
+	_core.layer.cornerRadius = radius;
+	[self addSubview:_core];
+
+	self.userInteractionEnabled = NO;
+
+	return self;
+}
+
+- (void)setCoreCollor:(UIColor *)coreColor
+{
+	_core.backgroundColor = coreColor;
+}
+
+- (void)setForceCollor:(UIColor *)forceColor
+{
+	_force.backgroundColor = forceColor;
+}
+
+- (void)setForceRadius:(CGFloat)forceRadius
+{
+	CGFloat scale = 1.0 + 0.6 * forceRadius;
+	_force.transform = CGAffineTransformMakeScale(scale, scale);
+}
+
+@end
+
+// MARK: DMZTouchEntity
+
 @interface DMZTouchEntity : NSObject
 
 @property (nonatomic, strong) UITouch *touch;
-@property (nonatomic, strong) UIView *view;
+@property (nonatomic, strong) DMZTouchView *view;
 
 @end
 
@@ -18,13 +77,12 @@
 
 @end
 
-@interface DMZTouchesWindow ()
-
-@property (nonatomic, strong) NSMutableArray<DMZTouchEntity *> *dmz_views;
-
-@end
+// MARK: DMZTouchesWindow
 
 @implementation DMZTouchesWindow
+{
+	NSMutableArray<DMZTouchEntity *> *_dmz_views;
+}
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
@@ -58,47 +116,47 @@
 {
 	_dmz_touchesEnabled = dmz_touchesEnabled;
 
-	if (_dmz_touchesEnabled && self.dmz_views == nil)
+	if (_dmz_touchesEnabled && _dmz_views == nil)
 	{
-		self.dmz_views = [[NSMutableArray alloc] init];
+		_dmz_views = [[NSMutableArray alloc] init];
 	}
-	else if (!_dmz_touchesEnabled && self.dmz_views != nil)
+	else if (!_dmz_touchesEnabled && _dmz_views != nil)
 	{
-		for (DMZTouchEntity *touchEntity in self.dmz_views)
+		for (DMZTouchEntity *touchEntity in _dmz_views)
 		{
 			[touchEntity.view removeFromSuperview];
 		}
 
-		self.dmz_views = nil;
+		_dmz_views = nil;
 	}
 }
 
 - (void)sendEvent:(UIEvent *)event
 {
-	if (self.dmz_views != nil)
+	if (_dmz_views != nil)
 	{
 		NSMutableSet *beganTouches = [NSMutableSet set];
-		NSMutableSet *movedTouches = [NSMutableSet set];
+
 		NSMutableSet *endedTouches = [NSMutableSet set];
 
 		for (UITouch *touch in event.allTouches)
 		{
-			if (touch.phase == UITouchPhaseBegan)
-			{
-				[beganTouches addObject:touch];
-			}
-			else if (touch.phase == UITouchPhaseMoved || touch.phase == UITouchPhaseStationary)
-			{
-				[movedTouches addObject:touch];
-			}
-			else
-			{
-				[endedTouches addObject:touch];
+			switch (touch.phase) {
+				case UITouchPhaseBegan:
+					[beganTouches addObject:touch];
+					break;
+				case UITouchPhaseEnded:
+				case UITouchPhaseCancelled:
+					[endedTouches addObject:touch];
+					break;
+				case UITouchPhaseMoved:
+				case UITouchPhaseStationary:
+					break;
 			}
 		}
 
 		[self dmz_touchesBegan:[beganTouches copy]];
-		[self dmz_touchesMoved:[movedTouches copy]];
+		[self dmz_touchesMoved:[event.allTouches copy]];
 		[self dmz_touchesEnded:[endedTouches copy]];
 	}
 
@@ -107,7 +165,7 @@
 
 - (DMZTouchEntity *)dmz_touchEntityWithTouch:(UITouch *)touch
 {
-	for (DMZTouchEntity *touchEntity in self.dmz_views)
+	for (DMZTouchEntity *touchEntity in _dmz_views)
 	{
 		if (touchEntity.touch == touch)
 		{
@@ -122,20 +180,24 @@
 {
 	for (UITouch *touch in touches)
 	{
-		DMZTouchEntity *touchEntity = [[DMZTouchEntity alloc] init];
-		touchEntity.touch = touch;
+		UIColor *forceColor = self.dmz_touchesForceColor;
+		if (forceColor == nil)
+		{
+			CGFloat red, green, blue, alpha;
+			[self.dmz_touchesColor getRed:&red green:&green blue:&blue alpha:&alpha];
+			forceColor = [UIColor colorWithRed:red green:green blue:blue alpha:alpha / 2];
+		}
 
-		UIView *view = [[UIView alloc] init];
-		view.frame = CGRectMake(0, 0, self.dmz_touchesRadius * 2, self.dmz_touchesRadius * 2);
-		view.layer.masksToBounds = YES;
-		view.layer.cornerRadius = self.dmz_touchesRadius;
-		view.backgroundColor = self.dmz_touchesColor;
-		view.center = [touch locationInView:self];
+		DMZTouchView *view = [[DMZTouchView alloc] initWithRadius:self.dmz_touchesRadius];
+		[view setCoreCollor:self.dmz_touchesColor];
+		[view setForceCollor:forceColor];
 		view.layer.zPosition = FLT_MAX;
 
+		DMZTouchEntity *touchEntity = [[DMZTouchEntity alloc] init];
+		touchEntity.touch = touch;
 		touchEntity.view = view;
 
-		[self.dmz_views addObject:touchEntity];
+		[_dmz_views addObject:touchEntity];
 
 		[self addSubview:view];
 	}
@@ -145,8 +207,16 @@
 {
 	for (UITouch *touch in touches)
 	{
+		CGFloat forceRadius = 0;
+		if (!self.dmz_touchesForceDisabled)
+		{
+			forceRadius = (touch.force - 0.5) / (touch.maximumPossibleForce - 0.5);
+			forceRadius = MAX(0, forceRadius);
+		}
+
 		DMZTouchEntity *touchEntity = [self dmz_touchEntityWithTouch:touch];
 		touchEntity.view.center = [touchEntity.touch locationInView:self];
+		[touchEntity.view setForceRadius:forceRadius];
 	}
 }
 
@@ -156,7 +226,7 @@
 	{
 		DMZTouchEntity *touchEntity = [self dmz_touchEntityWithTouch:touch];
 		[touchEntity.view removeFromSuperview];
-		[self.dmz_views removeObject:touchEntity];
+		[_dmz_views removeObject:touchEntity];
 	}
 }
 
