33e892a6282c410ea02c3672069901fa5d26aca7
Denis Morozov <denis.moroz.morozov@gmail.com>
2022-07-31 14:13:24 +0300

    Include touches as package

--- DMZTouchesWindowSample/swift/DMZTouchesWindowSample/AppDelegate.swift
+++ DMZTouchesWindowSample/swift/DMZTouchesWindowSample/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import DMZTouchesWindow
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -15,12 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
 	{
-//		let window = DMZTouchesWindow(frame: UIScreen.main.bounds);
-//		window.dmz_touchesEnabled = true
-//
-//		self.window = window
-//		self.window?.rootViewController = ViewController();
-//		self.window?.makeKeyAndVisible()
+		let window = DMZTouchesWindow(frame: UIScreen.main.bounds);
+		window.dmz_touchesEnabled = true
+
+		self.window = window
+		self.window?.rootViewController = ViewController();
+		self.window?.makeKeyAndVisible()
 
 		return true
 	}
